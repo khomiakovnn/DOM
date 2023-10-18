d8ac64a0d180ffc409c81e037aa655cd097cc724
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-10-18 17:13:28 -0400

    logic done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-import './css/style.css'
-import './js/app.js'
+let randomCell = Math.floor(Math.random() * 16) + 1;
+document.getElementById(randomCell).className = 'selected_cell';
+let activCell = randomCell
+
+setInterval(() => {
+    randomCell = Math.floor(Math.random() * 16) + 1
+    while (randomCell == activCell) {
+        randomCell = Math.floor(Math.random() * 16) + 1
+    }
+
+    document.getElementById(activCell).className = 'cell'
+    document.getElementById(randomCell).className = 'selected_cell'
+    activCell = randomCell
+}, 1000)
